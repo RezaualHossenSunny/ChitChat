3765d268e9de340cf8a59574b2ea7d3a223e022e
RezaualHossenSunny <sunnyrahman542@gmail.com>
2024-10-06 20:15:41 +0600

    loty animation add

--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -1,30 +1,28 @@
-import React from 'react'
-import Regicomp from '../Component/Registrasion/Index'
-import { ToastContainer, toast } from 'react-toastify';
+import React from "react";
+import Regicomp from "../Component/Registrasion/Index";
+import { ToastContainer, toast } from "react-toastify";
+import Lottie from "lottie-react";
+import regiani from '../animation/regi.ani.json'
 const Registration = () => {
   return (
     <>
-    <ToastContainer />
-    <div className="w-full h-screen flex items-center justify-center">
+      <ToastContainer />
+      <div className="w-full h-screen flex items-center justify-center">
         <div className="w-2/4 bg-white shadow-md rounded-md  p-4 flex items-center gap-x-10 justify-between">
+          {/* img */}
+          <div className=" hidden md:block  w-[45%]">
+            <Lottie animationData={regiani} loop={true} />
+          </div>
 
-        {/* img */}
-        <div className=" hidden md:block  w-[45%]">
-            <img src="https://t3.ftcdn.net/jpg/02/36/49/20/360_F_236492084_U8ueetasjJBnQcB6QlnDCHlVjVRObQgw.jpg" alt="" />
+          {/* rgi */}
+
+          <div className="w-[45%]">
+            <Regicomp toast={toast} />
+          </div>
         </div>
-
-
-        {/* rgi */}
-
-        <div  className="w-[45%]">
-            <Regicomp toast={toast}/>
-        </div>
-
-        </div>
-
-    </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Registration
+export default Registration;
