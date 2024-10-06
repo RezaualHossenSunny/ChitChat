fc7dbb098dfcbf7c68fa7124cea1d2e08d0874b4
RezaualHossenSunny <sunnyrahman542@gmail.com>
2024-10-06 20:03:44 +0600

    regi page regex add

--- src/Validate/Validation.jsx
+++ src/Validate/Validation.jsx
@@ -1,8 +1,10 @@
-import * as Yup from 'yup';
+import * as Yup from "yup";
 
-
-export const signUp  = Yup.object({
-    fullName: Yup.string().min(2).max(20).required("please enter your full name"),
-    email: Yup.string().email().required("enter your email"),
-    password: Yup.string().min(7).required("plaese enter your password"),
+export const signUp = Yup.object({
+  fullName: Yup.string().min(2).max(20).required("please enter your full name"),
+  email: Yup.string().email().required("enter your email"),
+  password: Yup.string()
+    .min(7)
+    .matches(/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/,"please enter one spical chacter")
+    .required("plaese enter your password"),
 });
