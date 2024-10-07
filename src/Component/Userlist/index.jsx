d5727f5a05368df799d26cbf2632f0159faf5348
RezaualHossenSunny <sunnyrahman542@gmail.com>
2024-10-07 23:53:47 +0600

    all user degain part done

---
+++ src/Component/Userlist/index.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { IoMdPersonAdd } from "react-icons/io";
+const Userlist = () => {
+  return (
+    <>
+      <div className="px-4">
+        <h1 className="font-roboto text-xl  py-4 font-semibold ">All Users</h1>
+        <div className="flex items-center justify-between mt-4">
+          <div className="flex items-center gap-x-2">
+            <div className="w-12 h-12 bg-slate-500 rounded-full overflow-hidden"></div>
+            <h3 className="font-roboto text-lg">Sunny </h3>
+          </div>
+          <div>
+          <IoMdPersonAdd className="text-2xl cursor-pointer" />
+          </div>
+        </div>
+        <div className="flex items-center justify-between mt-4">
+          <div className="flex items-center gap-x-2">
+            <div className="w-12 h-12 bg-slate-500 rounded-full overflow-hidden"></div>
+            <h3 className="font-roboto text-lg">Sunny </h3>
+          </div>
+          <div>
+          <IoMdPersonAdd className="text-2xl cursor-pointer" />
+          </div>
+        </div>
+
+        <div className="flex items-center justify-between mt-4">
+          <div className="flex items-center gap-x-2">
+            <div className="w-12 h-12 bg-slate-500 rounded-full overflow-hidden"></div>
+            <h3 className="font-roboto text-lg">Sunny </h3>
+          </div>
+          <div>
+          <IoMdPersonAdd className="text-2xl cursor-pointer" />
+          </div>
+        </div>
+        
+
+
+        
+      </div>
+    </>
+  );
+};
+
+export default Userlist;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import Logeduserourter from "../PrivateRout/Logeduserourter";
 import Navbar from "../Component/Navbar/Navbar";
+import Userlist from "../Component/Userlist";
 
 const Home = () => {
   return (
     <>
-      
+      <div className="grid  grid-cols-[1.5fr,4fr] px-5">
+       <div className=" w-full ">
+       <Userlist/>
+       </div>
+
+
+       <div className="bg-orange-500 h-full w-full ">f</div>
+     
+      </div>
 
    
     </>
