6c4adef4f93535bec33763e7b5279de0b3080267
RezaualHossenSunny <sunnyrahman542@gmail.com>
2024-10-08 00:30:44 +0600

    Friend requests degain done

--- src/Rotlayout/index.jsx
+++ src/Rotlayout/index.jsx
@@ -8,7 +8,7 @@ function Rotlayout() {
       <div className="relative w-full h-screen">
       <div className="h-[400px] w-full bg-[#4A81D3]">
 
-        <div className="w-3/4 bg-white shadow-md rounded-md absolute  top-2/4 left-1/2 -translate-y-2/4 -translate-x-1/2 h-[500px] ">
+        <div className="w-3/4 bg-white shadow-md rounded-md absolute  top-2/4 left-1/2 -translate-y-2/4 -translate-x-1/2  ">
 
         <Navbar/>
         <Outlet/>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Logeduserourter from "../PrivateRout/Logeduserourter";
 import Navbar from "../Component/Navbar/Navbar";
 import Userlist from "../Component/Userlist";
+import Friendreq from "../Component/Friendrequests";
+import Friend from "../Component/Friends";
 
 const Home = () => {
   return (
@@ -12,7 +14,15 @@ const Home = () => {
        </div>
 
 
-       <div className="bg-orange-500 h-full w-full ">f</div>
+       <div className=" h-full w-full  grid grid-cols-2 gap-x-9">
+        <div>
+          <Friendreq/>
+        </div>
+        <div>
+          <Friend/> 
+          </div>
+
+       </div>
      
       </div>
 
