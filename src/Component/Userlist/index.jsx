2ee9262f3559fa2b4d7387b736ffe0d3d863dc65
RezaualHossenSunny <sunnyrahman542@gmail.com>
2024-10-10 22:58:55 +0600

    use list  show

--- src/Component/Userlist/index.jsx
+++ src/Component/Userlist/index.jsx
@@ -1,39 +1,84 @@
-import React from "react";
+import { getDatabase, onValue, ref } from "firebase/database";
+import { getDownloadURL, getStorage, ref as Ref } from "firebase/storage";
+import React, { useEffect, useState } from "react";
 import { IoMdPersonAdd } from "react-icons/io";
+import { useSelector } from "react-redux";
+import avatart from '../../assets/Avatar.png'
 const Userlist = () => {
+  const user = useSelector((user) => user.login.login);
+  const [users,Setusers]=useState([]);
+
+
+  const db = getDatabase();
+
+  const storage = getStorage();
+
+
+
+
+
+
+
+  useEffect(()=>{
+    const starCountRef = ref(db, 'users/');
+    onValue(starCountRef, (snapshot) => {
+   const users =[]
+   snapshot.forEach((userlist)=>{
+    if(user.uid !== userlist.key){
+      getDownloadURL(Ref(storage, userlist.key))
+      .then((downloadURL) => {
+
+        users.push({
+          ... userlist.val(),
+          id: userlist.key,
+
+          photoURL:downloadURL
+        })
+
+      }).catch((error)=>{
+        users.push({
+          ... userlist.val(),
+          id: userlist.key,
+
+          photoURL:null
+        })
+      }).then(()=>{
+       Setusers([... users])
+      })
+    }
+   })
+     
+    });
+    
+    
+  },[db,user.uid, storage])
+
+console.log(users);
+
   return (
     <>
       <div className="p-4  h-[600px]  overflow-auto">
         <h1 className="font-roboto text-xl  py-4 font-semibold ">All Users</h1>
-        <div className="flex items-center justify-between mt-4">
-          <div className="flex items-center gap-x-2">
-            <div className="w-12 h-12 bg-slate-500 rounded-full overflow-hidden"></div>
-            <h3 className="font-roboto text-lg">Sunny </h3>
-          </div>
-          <div>
-          <IoMdPersonAdd className="text-2xl cursor-pointer" />
-          </div>
-        </div>
-        <div className="flex items-center justify-between mt-4">
-          <div className="flex items-center gap-x-2">
-            <div className="w-12 h-12 bg-slate-500 rounded-full overflow-hidden"></div>
-            <h3 className="font-roboto text-lg">Sunny </h3>
-          </div>
-          <div>
-          <IoMdPersonAdd className="text-2xl cursor-pointer" />
-          </div>
-        </div>
 
-        <div className="flex items-center justify-between mt-4">
-          <div className="flex items-center gap-x-2">
-            <div className="w-12 h-12 bg-slate-500 rounded-full overflow-hidden"></div>
-            <h3 className="font-roboto text-lg">Sunny </h3>
+        {
+          users.map((item)=>(
+            <div className="flex items-center justify-between mt-4">
+            <div className="flex items-center gap-x-2">
+              <div className="w-12 h-12 bg-sky-500 rounded-full overflow-hidden">{
+                <img src={item.photoURL || avatart}/>
+                }</div>
+              <h3 className="font-roboto text-lg">{item.username} </h3>
+            </div>
+            <div>
+            <IoMdPersonAdd className="text-2xl cursor-pointer" />
+            </div>
           </div>
-          <div>
-          <IoMdPersonAdd className="text-2xl cursor-pointer" />
-          </div>
-        </div>
+          ))
+        }
+     
         
+
+ 
 
 
         
