7bb1be0e9bb5452fa4822554c715e5b2aaa7529d
RezaualHossenSunny <sunnyrahman542@gmail.com>
2024-10-19 12:42:59 +0600

    day7

--- src/Component/Friendrequests/index.jsx
+++ src/Component/Friendrequests/index.jsx
@@ -1,81 +1,82 @@
-import { getDatabase, onValue, push, ref, remove, set } from 'firebase/database';
-import React, { useEffect, useState } from 'react'
-import { useSelector } from 'react-redux';
+import {
+  getDatabase,
+  onValue,
+  push,
+  ref,
+  remove,
+  set,
+} from "firebase/database";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import avatart from "../../assets/Avatar.png";
 const Friendreq = () => {
   const user = useSelector((state) => state.login.login);
   const [friendReq, setFriendReq] = useState([]);
   const db = getDatabase();
-  useEffect(()=>{
-
-
-    const starCountRef = ref(db, 'friendrequest/');
+  useEffect(() => {
+    const starCountRef = ref(db, "friendrequest/");
     onValue(starCountRef, (snapshot) => {
+      let friendreq = [];
 
-      let friendreq=[]
-
-      snapshot.forEach((item)=>{
-        if(user.uid === item.val().receiverId) {
-          friendreq.push({...item.val(), id:item.key})
+      snapshot.forEach((item) => {
+        if (user.uid === item.val().receiverId) {
+          friendreq.push({ ...item.val(), id: item.key });
         }
-      })
-      setFriendReq(friendreq)
+      });
+      setFriendReq(friendreq);
     });
-  },[db,user.uid])
-
+  }, [db, user.uid]);
 
   //acept req
 
-  const handleAccept=(data)=>{
+  const handleAccept = (data) => {
+    set(push(ref(db, "friends")), {
+      ...data,
+    }).then(() => {
+      remove(ref(db, "friendrequest/" + data.id));
+    });
+  };
 
-    set(push(ref(db,'friends')),{
-      ...data
-    }).then(()=>{
-      remove(ref(db,'friendrequest/' + data.id))
-    })
-    
+  //reject
 
-  } 
-
+  const handleReject = (data) => {
+    remove(ref(db, "friendrequest/" + data.id));
+  };
 
   return (
     <>
-    <div className='bg-white shadow-md p-4  md:h-[600px]  overflow-auto  ' >
-        <h1 className='font-roboto text-xl  py-4 font-semibold'>Friend requests</h1>
+      <div className="bg-white shadow-md p-4  md:h-[600px]  overflow-auto  ">
+        <h1 className="font-roboto text-xl  py-4 font-semibold">
+          Friend requests
+        </h1>
 
-      
-
-        {
-          friendReq ?.map((item)=>(
-
-            <div className='flex items-center justify-between mt-4' key={item.id}>
-
-            <div className='flex items-center gap-x-2'>
-                <div className='w-10 h-10 bg-orange-400 rounded-full overflow-hidden'>
-
+        {friendReq?.map((item) => (
+          <div className="flex items-center justify-between mt-4" key={item.id}>
+            <div className="flex items-center gap-x-2">
+              <div className="w-10 h-10 bg-orange-400 rounded-full overflow-hidden">
                 <img src={item.senderProfile || avatart} alt={item.username} />
-
-                </div>
-                <p className="font-roboto text-lg">{item.senderName} </p>
-
+              </div>
+              <p className="font-roboto text-lg">{item.senderName} </p>
             </div>
-            <div className='flex gap-x-2'>
-                <button className='py-3 px-5 rounded-xl w-24 bg-sky-400 font-roboto text-white font-medium' onClick={()=>handleAccept(item)}>Accept</button>
-                <button className='py-3 px-5 rounded-xl w-24 bg-red-400 font-roboto text-white font-medium'>Reject</button>
-                
-
+            <div className="flex gap-x-2">
+              <button
+                className="py-3 px-5 rounded-xl w-24 bg-sky-400 font-roboto text-white font-medium"
+                onClick={() => handleAccept(item)}
+              >
+                Accept
+              </button>
+              <button
+                className="py-3 px-5 rounded-xl w-24 bg-red-400 font-roboto text-white font-medium"
+                onClick={() => handleReject(item)}
+              >
+                Reject
+              </button>
             </div>
-
-        </div>
-          ))
-        }
-
-     
-
-      
-    </div>
+          </div>
+        ))}
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Friendreq
+export default Friendreq;
--- src/Component/Friends/index.jsx
+++ src/Component/Friends/index.jsx
@@ -1,34 +1,71 @@
-import React from "react";
+import { getDatabase, onValue, ref } from "firebase/database";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import { useLocation, useNavigate } from "react-router-dom";
-
+import avatart from "../../assets/Avatar.png";
 const Friend = () => {
+  const user = useSelector((state) => state.login.login);
+  const [friend, Setfriend] = useState([]);
   const locataion = useLocation();
-  const navagiate =useNavigate()
+  const navagiate = useNavigate();
+  const db = getDatabase();
+
+  useEffect(() => {
+    const starCountRef = ref(db, "friends/");
+    onValue(starCountRef, (snapshot) => {
+      let friendArr = [];
+
+      snapshot.forEach((item) => {
+        if (
+          user.uid === item.val().senderId ||
+          user.uid === item.val().receiverId
+        ) {
+          friendArr.push({ ...item.val(), id: item.key });
+        }
+      });
+
+      Setfriend(friendArr);
+    });
+  }, [db, user.uid]);
+
+
   return (
     <>
       <div className="bg-white shadow-md p-4  h-[600px]  overflow-auto">
         <h1 className="font-roboto text-xl  py-4 font-semibold">Friends</h1>
 
-        <div className="flex items-center justify-between mt-4">
+        
+        {
+          friend?.map((item)=>(
+             <div className="flex items-center justify-between mt-4" key={item.id}>
           <div className="flex items-center gap-x-2">
-            <div className="w-10 h-10 bg-gray-400 rounded-full overflow-hidden"></div>
-            <p className="font-roboto text-lg">RH Sunny</p>
+            <div className="w-10 h-10 bg-gray-400 rounded-full overflow-hidden">
+              {
+                user.uid === item.receiverId ? <img src={item.senderProfile || avatart} alt={item.username} /> : <img src={item.receiverProfile || avatart} alt={item.username} />
+              }
+            
+            </div>
+            <p className="font-roboto text-lg">{
+              user.uid === item.senderId ? item.receiverName : item.senderName
+              }</p>
           </div>
           <div className="">
-
-           {
-            locataion.pathname =="/" &&
-            <button onClick={()=>{
-              navagiate('/message')
-            }} className="py-3 px-5 rounded-xl w-24 bg-sky-500 font-roboto text-white font-medium">
-            Message
-          </button>
-           }
+            {locataion.pathname == "/" && (
+              <button
+                onClick={() => {
+                  navagiate("/message");
+                }}
+                className="py-3 px-5 rounded-xl w-24 bg-sky-500 font-roboto text-white font-medium"
+              >
+                Message
+              </button>
+            )}
           </div>
         </div>
-        
+          ))
+        }
 
-    
+
       </div>
     </>
   );
